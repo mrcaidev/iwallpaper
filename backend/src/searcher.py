ce7d7e928432125ce0ad4cd2d01e63fd19fea526
mrcaidev <mrcaidev@gmail.com>
2023-07-20 14:06:55 +0800

    feat: add optional quantity param for /search

--- backend/src/searcher.py
+++ backend/src/searcher.py
@@ -2,6 +2,7 @@ import logging
 from typing import Annotated
 
 from fastapi import APIRouter, Query
+from pydantic import PositiveInt
 
 from .supabase import supabase_client
 from .vectorizer import vectorizer
@@ -10,13 +11,21 @@ router = APIRouter(prefix="/search")
 
 
 @router.get("/")
-def search(query: Annotated[str, Query(min_length=1, max_length=50)]):
+def search(
+    query: Annotated[str, Query(min_length=1, max_length=50)],
+    quantity: PositiveInt = 20,
+):
     query_vector = vectorizer.transform([query])[0]
 
     wallpapers = (
-        supabase_client.rpc("search_wallpapers", {"query": query_vector}).execute().data
+        supabase_client.rpc(
+            "search_wallpapers",
+            {"query": query_vector, "quantity": quantity},
+        )
+        .execute()
+        .data
     )
 
-    logging.info(f"Found {len(wallpapers)} search results for: {query}")
+    logging.info(f"Found {len(wallpapers)} search results")
 
     return {"message": "", "data": wallpapers}
