37b0de3759c1559fb1f0519ecd99c5ac4cb680a8
mrcaidev <mrcaidev@gmail.com>
2023-08-09 15:14:13 +0800

    style: return error object

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -3,14 +3,14 @@ import { ZodError } from "zod";
 
 export function generateErrorResponse(error: unknown) {
   if (error instanceof ZodError) {
-    return NextResponse.json({ message: "Malformed data" }, { status: 400 });
+    return NextResponse.json({ error: "Malformed data" }, { status: 400 });
+  }
+
+  if (error instanceof Error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 
   console.error(error);
 
-  if (error instanceof Error) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
-  }
-
-  return NextResponse.json({ message: "Unknown error" }, { status: 500 });
+  return NextResponse.json({ error: "Unknown error" }, { status: 500 });
 }
