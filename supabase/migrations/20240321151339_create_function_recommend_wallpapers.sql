86a378fc6623d3828356374bf5bfb45557ce9e02
Yuwang Cai <mrcaidev@gmail.com>
2024-03-27 16:54:57 +0800

    fix: select distinct wallpapers in recommendation

--- supabase/migrations/20240321151339_create_function_recommend_wallpapers.sql
+++ supabase/migrations/20240321151339_create_function_recommend_wallpapers.sql
@@ -19,20 +19,23 @@ DECLARE
 BEGIN
   recommended_ids = ARRAY(
     (
-      SELECT (candidates->>'id')::UUID
+      SELECT DISTINCT id
       FROM (
-        SELECT UNNEST(wallpapers.most_similar_wallpapers) AS candidates, histories.rating
-        FROM wallpapers
-        RIGHT OUTER JOIN histories on wallpapers.id = histories.wallpaper_id
-        WHERE histories.user_id = auth.uid()
-          AND histories.rating IS NOT NULL
-      ) AS subquery
-      WHERE (candidates->>'id')::UUID NOT IN (
-        SELECT wallpaper_id
-        FROM histories
-        WHERE user_id = auth.uid()
-      )
-      ORDER BY rating * (candidates->>'similarity')::FLOAT DESC
+        SELECT (candidates->>'id')::UUID AS id
+        FROM (
+          SELECT UNNEST(wallpapers.most_similar_wallpapers) AS candidates, histories.rating
+          FROM wallpapers
+          RIGHT OUTER JOIN histories on wallpapers.id = histories.wallpaper_id
+          WHERE histories.user_id = auth.uid()
+            AND histories.rating IS NOT NULL
+        ) AS candidates_subquery
+        WHERE (candidates->>'id')::UUID NOT IN (
+          SELECT wallpaper_id
+          FROM histories
+          WHERE user_id = auth.uid()
+        )
+        ORDER BY rating * (candidates->>'similarity')::FLOAT DESC
+      ) AS rank_subquery
       LIMIT quantity
     )
     UNION ALL
