15ed7babc8d7a877ab616db07e871b4ebef4117e
Yuwang Cai <mrcaidev@gmail.com>
2024-04-03 19:16:49 +0800

    style: use SUPABASE_URL on server side

--- app/supabase/server.ts
+++ app/supabase/server.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
 export const supabaseServer = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.SUPABASE_URL ?? "",
   process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
 );
