4e87681de5257346356b132f80a640aac92ad76f
Yuwang Cai <mrcaidev@gmail.com>
2024-04-06 10:54:45 +0800

    feat: confirm email api

---
+++ app/api/auth/confirm/route.ts
@@ -0,0 +1,34 @@
+import { type EmailOtpType } from "@supabase/supabase-js";
+import { NextResponse, type NextRequest } from "next/server";
+import { createServerSupabaseClient } from "utils/supabase/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const tokenHash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/";
+
+  if (!tokenHash || !type) {
+    return NextResponse.json({ message: "Malformed link" }, { status: 400 });
+  }
+
+  const supabase = createServerSupabaseClient();
+
+  const { error } = await supabase.auth.verifyOtp({
+    type,
+    token_hash: tokenHash,
+  });
+
+  if (error) {
+    const message = error.message;
+    return NextResponse.json({ message }, { status: 500 });
+  }
+
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
+  redirectTo.searchParams.delete("token_hash");
+  redirectTo.searchParams.delete("type");
+  redirectTo.searchParams.delete("next");
+
+  return NextResponse.redirect(redirectTo);
+}
