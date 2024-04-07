e9d70c6af4bd7a8056f61dbe7e3b471f8a89026b
Yuwang Cai <mrcaidev@gmail.com>
2024-04-07 12:05:56 +0800

    fix: layout content overlap

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,7 +4,7 @@ import { UserMenu } from "./user-menu";
 
 export function Header() {
   return (
-    <div className="flex items-center gap-4 w-full px-4 lg:px-6 py-2 lg:py-3 border-b bg-muted/40">
+    <div className="flex items-center gap-4 w-full px-4 lg:px-6 py-2 lg:py-3 border-b bg-muted/40 backdrop-blur">
       <MobileSidebar />
       <div className="grow relative">
         <SearchBox />
