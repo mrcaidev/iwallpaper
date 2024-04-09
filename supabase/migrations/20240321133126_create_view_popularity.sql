727faa32b716243c157d33306430e654638a8137
Yuwang Cai <mrcaidev@gmail.com>
2024-04-09 19:50:33 +0800

    refactor: construct popularities with join and group by

--- supabase/migrations/20240321133126_create_view_popularity.sql
+++ supabase/migrations/20240321133126_create_view_popularity.sql
@@ -1,9 +1,7 @@
 CREATE VIEW popularities AS (
-  SELECT wallpapers.id, (
-    SELECT COUNT(histories.user_id)
-    FROM histories
-    WHERE histories.wallpaper_id = wallpapers.id
-      AND histories.preference >= 3
-  ) AS popularity
-  FROM wallpapers
+  SELECT wallpapers.id, COUNT(*) AS popularity
+  FROM histories
+  LEFT OUTER JOIN wallpapers ON histories.wallpaper_id = wallpapers.id
+  WHERE histories.preference >= 3
+  GROUP BY wallpapers.id
 );
