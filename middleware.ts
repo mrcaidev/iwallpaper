1747843f6b7ce15134d4e7506de1656ed2893623
Yuwang Cai <mrcaidev@gmail.com>
2024-04-10 14:26:32 +0800

    feat: middleware auth guard

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,69 @@
-import { updateSession } from "middlewares/update-session";
-import type { NextRequest } from "next/server";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+import type { Database } from "utils/supabase/types";
+
+const authenticatedRegexs = ["^/favorites", "^/setting"];
+
+const unauthenticatedRegexs = ["^/sign-in", "^/sign-up", "^/forgot-password"];
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  let response = NextResponse.next({
+    request: { headers: request.headers },
+  });
+
+  const supabase = createServerClient<Database>(
+    process.env.SUPABASE_URL ?? "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({ name, value, ...options });
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
+          response.cookies.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({ name, value: "", ...options });
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
+          response.cookies.set({ name, value: "", ...options });
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const isAuthenticatedPathname = authenticatedRegexs.some((regex) =>
+    request.nextUrl.pathname.match(regex),
+  );
+  if (isAuthenticatedPathname && !user) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/sign-in";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  const isUnauthenticatedPathname = unauthenticatedRegexs.some((regex) =>
+    request.nextUrl.pathname.match(regex),
+  );
+  if (isUnauthenticatedPathname && user) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return response;
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
