4d08a78ad007ba55451af2f8e4ee09f188011284
Yuwang Cai <mrcaidev@gmail.com>
2024-04-17 23:12:30 +0800

    refactor: remove table profiles

--- supabase/migrations/20240321130233_create_table_profiles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE profiles (
-  id UUID PRIMARY KEY REFERENCES auth.users ON DELETE CASCADE,
-  nickname TEXT,
-  avatar_url TEXT
-);
-
-ALTER TABLE profiles ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "Users can select all profiles." ON profiles FOR SELECT USING (TRUE);
