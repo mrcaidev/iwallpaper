2458a0b077c1273ddd3aa3146c232b9957ee8884
Yuwang Cai <mrcaidev@gmail.com>
2024-04-22 22:22:24 +0800

    refactor: use class to implement search engine

--- backend/src/searcher.py
+++ backend/src/searcher.py
@@ -1,229 +1,125 @@
 import re
 from collections import defaultdict
 
-import numpy as np
 import pandas as pd
 
 from .gte import gte_model
 
 
-def normalize_text(text: str):
-    text = text.lower()
-    text = re.sub(r"[^a-z0-9\s]", " ", text)
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
+class SearchEngine:
+    def __init__(self, dataset_path: str):
+        self._load_dataset(dataset_path)
+        self._build_inverted_index()
+        self._build_embedding_database()
 
+    def _load_dataset(self, filepath: str):
+        self.dataset = pd.read_csv(filepath, index_col="movieId")
 
-def load_dataset(filepath: str):
-    dataset = pd.read_csv(filepath, index_col="movieId")
-    dataset["content"] = (
-        dataset["title"].apply(normalize_text)
-        + " "
-        + dataset["genres"].apply(normalize_text)
-    )
-    dataset.drop(columns=["title", "genres"], inplace=True)
-    return dataset
+    @staticmethod
+    def _parse_words(text: str):
+        text = text.lower()
+        text = re.sub(r"[^a-z0-9\s]", " ", text)
+        return set(text.split())
 
+    def _build_inverted_index(self):
+        inverted_index = defaultdict(list[int])
 
-def build_inverted_index(dataset: pd.DataFrame):
-    inverted_index = defaultdict(list)
+        for id, row in self.dataset.iterrows():
+            words = SearchEngine._parse_words(row["title"])
+            for word in words:
+                inverted_index[word].append(id)
 
-    for id, row in dataset.iterrows():
-        for word in set(row["content"].split()):
-            inverted_index[word].append(id)
+        self.inverted_index = inverted_index
 
-    return inverted_index
+    def _build_embedding_database(self):
+        sentences = self.dataset["title"].to_list()
+        embeddings = gte_model.encode(sentences, normalize_embeddings=True)
+        self.embedding_database = pd.DataFrame(embeddings, index=self.dataset.index)
 
+    def _build_ranking(self, name: str, ids: list[int], top_k: int | None = None):
+        ids = ids[:top_k] if top_k else ids
+        return self.dataset.loc[ids, "title"].rename(name)
 
-def keyword_search(
-    inverted_index: dict[str, list[int]],
-    query: str,
-    top_k: int | None = None,
-):
-    normalized_query = normalize_text(query)
-    query_words = set(normalized_query.split())
+    def keyword_search(self, query: str, top_k: int | None = None):
+        query_words = SearchEngine._parse_words(query)
 
-    scoreboard = defaultdict(float)
+        scoreboard = defaultdict(float)
 
-    for word in query_words:
-        word_ids = inverted_index.get(word, [])
-        word_weight = 1 / len(word_ids) if word_ids else 0
+        for word in query_words:
+            word_ids = self.inverted_index.get(word, [])
+            word_weight = 1 / len(word_ids) if word_ids else 0
 
-        for id in word_ids:
-            scoreboard[id] += word_weight
+            for id in word_ids:
+                scoreboard[id] += word_weight
 
-    ranking = sorted(scoreboard.keys(), key=lambda id: (-scoreboard[id], id))
-    return ranking[:top_k] if top_k else ranking
+        ids = sorted(scoreboard.keys(), key=lambda id: (-scoreboard[id], id))
+        return self._build_ranking("Keyword Search", ids, top_k=top_k)
 
+    def semantic_search(
+        self,
+        query: str,
+        top_k: int | None = None,
+        similarity_threshold: float = 0.82,
+    ):
+        query_embedding = gte_model.encode(query, normalize_embeddings=True)
 
-def build_embedding_database(dataset: pd.DataFrame):
-    matrix = gte_model.encode(dataset["content"].tolist(), normalize_embeddings=True)
-    assert isinstance(matrix, np.ndarray)
-    return pd.DataFrame(matrix, index=dataset.index)
+        similarities: pd.Series = self.embedding_database @ query_embedding
 
+        ids = (
+            similarities[similarities > similarity_threshold]
+            .sort_values(ascending=False)
+            .index.to_list()
+        )
+        return self._build_ranking("Semantic Search", ids, top_k=top_k)
 
-def semantic_search(
-    embedding_database: pd.DataFrame,
-    query: str,
-    top_k: int | None = None,
-    similarity_threshold: float = 0.85,
-):
-    normalized_query = normalize_text(query)
-    query_embedding = gte_model.encode(normalized_query, normalize_embeddings=True)
+    def reciprocal_rank_fusion(
+        self,
+        *rankings: pd.Series,
+        top_k: int | None = None,
+        ranking_weights: list[float] | None = None,
+        smoothing_factor: int = 1,
+    ):
+        if not ranking_weights:
+            ranking_weights = [1.0] * len(rankings)
 
-    similarities: pd.Series = embedding_database @ query_embedding
+        assert len(rankings) == len(ranking_weights)
 
-    ranking = (
-        similarities[similarities > similarity_threshold]
-        .sort_values(ascending=False)
-        .index.tolist()
-    )
-    return ranking[:top_k] if top_k else ranking
+        scoreboard = defaultdict(float)
 
+        for ranking_index, ranking in enumerate(rankings):
+            ranking_weight = ranking_weights[ranking_index]
+            for rank, id in enumerate(ranking.index):
+                scoreboard[id] += ranking_weight / (smoothing_factor + rank + 1)
 
-def reciprocal_rank_fusion(
-    *rankings: list[int],
-    ranking_weights: list[float] | None = None,
-    smoothing_factor: int = 1,
-    top_k: int | None = None,
-):
-    if not ranking_weights:
-        ranking_weights = [1.0] * len(rankings)
+        ids = sorted(scoreboard.keys(), key=lambda id: (-scoreboard[id], id))
+        return self._build_ranking("Hybrid Search", ids, top_k=top_k)
 
-    assert len(rankings) == len(ranking_weights)
+    def generate_report(self, query: str, top_k: int = 30):
+        keyword_ranking = self.keyword_search(query)
+        semantic_ranking = self.semantic_search(query)
 
-    scoreboard = defaultdict(float)
+        possible_num = len(set([*keyword_ranking.index, *semantic_ranking.index]))
+        expected_num = min(top_k, possible_num)
 
-    for ranking_index, ranking in enumerate(rankings):
-        ranking_weight = ranking_weights[ranking_index]
-        for rank, id in enumerate(ranking):
-            scoreboard[id] += ranking_weight / (smoothing_factor + rank + 1)
+        keyword_ranking = keyword_ranking[:top_k]
+        semantic_ranking = semantic_ranking[:top_k]
+        hybrid_ranking = self.reciprocal_rank_fusion(
+            keyword_ranking, semantic_ranking, top_k=top_k
+        )
 
-    ranking = sorted(scoreboard.keys(), key=lambda id: (-scoreboard[id], id))
-    return ranking[:top_k] if top_k else ranking
+        keyword_ranking.reset_index(drop=True, inplace=True)
+        semantic_ranking.reset_index(drop=True, inplace=True)
+        hybrid_ranking.reset_index(drop=True, inplace=True)
 
+        report = pd.concat([keyword_ranking, semantic_ranking, hybrid_ranking], axis=1)
+        report.index += 1
 
-def is_query_matched(query: str, target: str):
-    query_words = set(query.split())
-    return any(query_word in target for query_word in query_words)
-
-
-def search_test(
-    dataset: pd.DataFrame,
-    inverted_index: dict[str, list[int]],
-    embedding_database: pd.DataFrame,
-    query: str,
-    top_k: int = 20,
-):
-    keyword_ranking = keyword_search(inverted_index, query)
-    semantic_ranking = semantic_search(
-        embedding_database,
-        query,
-        similarity_threshold=0.83,
-    )
-    rrf_ranking = reciprocal_rank_fusion(keyword_ranking, semantic_ranking)
-
-    possible_correct_num = len(set().union(keyword_ranking, semantic_ranking))
-    expected_correct_num = min(top_k, possible_correct_num)
-
-    keyword_ranking = keyword_ranking[:top_k]
-    semantic_ranking = semantic_ranking[:top_k]
-    rrf_ranking = rrf_ranking[:top_k]
-
-    keyword_given_num = len(keyword_ranking)
-    semantic_given_num = len(semantic_ranking)
-    rrf_given_num = len(rrf_ranking)
-
-    normalized_query = normalize_text(query)
-    keyword_correct_num = list(
-        is_query_matched(normalized_query, str(dataset.loc[id, "content"]))
-        for id in keyword_ranking
-    ).count(True)
-    semantic_correct_num = list(
-        is_query_matched(normalized_query, str(dataset.loc[id, "content"]))
-        for id in semantic_ranking
-    ).count(True)
-    rrf_correct_num = list(
-        is_query_matched(normalized_query, str(dataset.loc[id, "content"]))
-        for id in rrf_ranking
-    ).count(True)
-
-    return pd.Series(
-        {
-            "POS": possible_correct_num,
-            "EXP": expected_correct_num,
-            "KS_COR": keyword_correct_num,
-            "KS_GVN": keyword_given_num,
-            "SS_COR": semantic_correct_num,
-            "SS_GVN": semantic_given_num,
-            "HS_COR": rrf_correct_num,
-            "HS_GVN": rrf_given_num,
-        },
-        dtype=int,
-        name=query,
-    )
-
-
-def generate_report(outcome: pd.DataFrame):
-    outcome.loc["TOTAL"] = outcome.sum()
-
-    keyword_precision = outcome.loc["TOTAL", "KS_COR"] / outcome.loc["TOTAL", "KS_GVN"]
-    keyword_recall = outcome.loc["TOTAL", "KS_COR"] / outcome.loc["TOTAL", "EXP"]
-    keyword_f1 = (
-        2 * keyword_precision * keyword_recall / (keyword_precision + keyword_recall)
-    )
-
-    semantic_precision = outcome.loc["TOTAL", "SS_COR"] / outcome.loc["TOTAL", "SS_GVN"]
-    semantic_recall = outcome.loc["TOTAL", "SS_COR"] / outcome.loc["TOTAL", "EXP"]
-    semantic_f1 = (
-        2
-        * semantic_precision
-        * semantic_recall
-        / (semantic_precision + semantic_recall)
-    )
-
-    rrf_precision = outcome.loc["TOTAL", "HS_COR"] / outcome.loc["TOTAL", "HS_GVN"]
-    rrf_recall = outcome.loc["TOTAL", "HS_COR"] / outcome.loc["TOTAL", "EXP"]
-    rrf_f1 = 2 * rrf_precision * rrf_recall / (rrf_precision + rrf_recall)
-
-    print(outcome)
-    print()
-    print(f"Keyword Search Precision: {keyword_precision:.2f}")
-    print(f"Keyword Search Recall: {keyword_recall:.2f}")
-    print(f"Keyword Search F1: {keyword_f1:.2f}")
-    print()
-    print(f"Semantic Search Precision: {semantic_precision:.2f}")
-    print(f"Semantic Search Recall: {semantic_recall:.2f}")
-    print(f"Semantic Search F1: {semantic_f1:.2f}")
-    print()
-    print(f"Hybrid Search Precision: {rrf_precision:.2f}")
-    print(f"Hybrid Search Recall: {rrf_recall:.2f}")
-    print(f"Hybrid Search F1: {rrf_f1:.2f}")
+        print(report)
+        print(f"Possible correct = {possible_num}, Expected correct = {expected_num}")
 
 
 if __name__ == "__main__":
-    dataset = load_dataset("data/ml-latest-small/movies.csv")
-    inverted_index = build_inverted_index(dataset)
-    embedding_database = build_embedding_database(dataset)
+    search_engine = SearchEngine("data/ml-latest-small/movies.csv")
 
-    queries = [
-        "toy",
-        "father",
-        "america",
-        "casino",
-        "castle",
-        "money",
-        "star",
-        "antonia",
-        "twilight",
-        "sugar",
-    ]
-
-    test_outcome = pd.concat(
-        [
-            search_test(dataset, inverted_index, embedding_database, query, top_k=30)
-            for query in queries
-        ],
-        axis=1,
-    ).T
-    generate_report(test_outcome)
+    while (query := input(">>> ")) != "q":
+        search_engine.generate_report(query)
