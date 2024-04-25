661a85ac31e8927821e847e93f115991188bd34e
Yuwang Cai <mrcaidev@gmail.com>
2024-04-25 12:14:53 +0800

    style: unified naming with python code

--- supabase/migrations/20240321135116_create_function_search_wallpapers.sql
+++ supabase/migrations/20240321135116_create_function_search_wallpapers.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION search_wallpapers(
   query_embedding VECTOR(384),
   take INTEGER = 30,
   skip INTEGER = 0,
-  full_text_weight FLOAT = 1.0,
+  keyword_weight FLOAT = 1.0,
   semantic_weight FLOAT = 1.0,
   rrf_k INTEGER = 1
 )
@@ -18,7 +18,7 @@ RETURNS TABLE (
 )
 LANGUAGE sql
 AS $$
-WITH full_text_ranks AS (
+WITH keyword_ranking AS (
   SELECT id, ROW_NUMBER() OVER (
     ORDER BY TS_RANK_CD(fts, websearch_to_tsquery('english', query)) DESC
   ) AS rank
@@ -26,7 +26,7 @@ WITH full_text_ranks AS (
   WHERE fts @@ websearch_to_tsquery('english', query)
   ORDER BY rank ASC
 ),
-semantic_ranks AS (
+semantic_ranking AS (
   SELECT id, ROW_NUMBER() OVER (
     ORDER BY embedding <#> query_embedding ASC
   ) AS rank
@@ -46,13 +46,13 @@ SELECT
   wallpapers.height,
   wallpapers.tags,
   personal_histories.attitude
-FROM full_text_ranks
-FULL OUTER JOIN semantic_ranks ON full_text_ranks.id = semantic_ranks.id
-LEFT OUTER JOIN wallpapers ON COALESCE(full_text_ranks.id, semantic_ranks.id) = wallpapers.id
+FROM keyword_ranking
+FULL OUTER JOIN semantic_ranking ON keyword_ranking.id = semantic_ranking.id
+LEFT OUTER JOIN wallpapers ON COALESCE(keyword_ranking.id, semantic_ranking.id) = wallpapers.id
 LEFT OUTER JOIN personal_histories ON wallpapers.id = personal_histories.wallpaper_id
 ORDER BY
-  COALESCE(1.0 / (rrf_k + full_text_ranks.rank), 0.0) * full_text_weight +
-  COALESCE(1.0 / (rrf_k + semantic_ranks.rank), 0.0) * semantic_weight
+  keyword_weight * COALESCE(1.0 / (rrf_k + keyword_ranking.rank), 0.0) +
+  semantic_weight * COALESCE(1.0 / (rrf_k + semantic_ranking.rank), 0.0)
   DESC
 LIMIT LEAST(take, 30)
 OFFSET skip
