3d8eed49142d962a6e18933d6f9a466eb265e37e
Yuwang Cai <mrcaidev@gmail.com>
2024-04-30 22:52:13 +0800

    feat: avatar upload size limit

--- supabase/migrations/20240321125939_create_bucket_avatars.sql
+++ supabase/migrations/20240321125939_create_bucket_avatars.sql
@@ -1,5 +1,5 @@
-INSERT INTO storage.buckets (id, name, public, allowed_mime_types)
-VALUES ('avatars', 'avatars', TRUE, '{"image/*"}');
+INSERT INTO storage.buckets (id, name, public, file_size_limit, allowed_mime_types)
+VALUES ('avatars', 'avatars', TRUE, 1024 * 1024, '{"image/*"}');
 
 CREATE POLICY "Users can insert their own avatar."
 ON storage.objects
