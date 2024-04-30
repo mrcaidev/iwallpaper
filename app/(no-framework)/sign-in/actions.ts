3eec19ec0c915545699aa7474a8421087f9bd898
Yuwang Cai <mrcaidev@gmail.com>
2024-04-30 17:18:19 +0800

    refactor: format

--- app/(no-framework)/sign-in/actions.ts
+++ app/(no-framework)/sign-in/actions.ts
@@ -10,10 +10,7 @@ export async function signIn(_: unknown, formData: FormData) {
   const email = formData.get("email")!.toString();
   const password = formData.get("password")!.toString();
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
 
   if (error) {
     return { error: error.message };
